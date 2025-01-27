d10ca50eaf3f951e3af210ce08be5384b09ad576
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-27 16:59:47 +0700

    summary finance component

--- client/src/components/shared/SummaryFinancial.jsx
+++ client/src/components/shared/SummaryFinancial.jsx
@@ -1,7 +1,27 @@
 export default function SummaryFinancial() {
     return (
         <diV>
-            <p>Summary Financial component</p>
+            <h1 className="mb-2">สรุปการเงิน</h1>
+            <div className="flex gap-5">
+                <div className="border-solid border-1 border-green-700 text-green-700 py-1 rounded-lg w-2/14">
+                    <div className="px-4">
+                        <p>รายรับ</p>
+                        <p className="text-3xl text-center">20,000</p>
+                    </div>
+                </div>
+                <div className="border-solid border-1 border-red-700 text-red-700 py-1 rounded-lg w-2/14">
+                    <div className="px-4">
+                        <p>รายจ่าย</p>
+                        <p className="text-3xl text-center">10,000,00</p>
+                    </div>
+                </div>
+                <div className="bg-gradient-to-r from-blue-500 to-orange-400 text-white py-1 rounded-lg w-2/14 font-bold">
+                    <div className="px-4">
+                        <p>คงเหลือ</p>
+                        <p className="text-3xl text-center">10,000</p>
+                    </div>
+                </div>
+            </div>
         </diV>
-    )
+    );
 }
--- client/src/pages/Boxs.jsx
+++ client/src/pages/Boxs.jsx
@@ -1,6 +1,9 @@
+import SummaryFinancial from "../components/shared/SummaryFinancial";
+
 export default function Boxs() {
     return (
-        <div>
+        <div className="container mx-auto mt-8">
+            <SummaryFinancial />
             <p>Boxs Page</p>
         </div>
     )
