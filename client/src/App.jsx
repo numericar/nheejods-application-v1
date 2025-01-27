758a0fd6a81e5e3602ef9e9bba5f29371c447a66
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-27 13:57:10 +0700

    add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Boxs from "./pages/Boxs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Transaction from "./pages/Transaction";
+import Navbar from "./components/shared/Navbar";
+
 function App() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <p>Hello World</p>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="" element={ <Boxs /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
+          <Route path="/box/:boxId" element={ <Transaction /> } />
+        z</Routes>   
+      </BrowserRouter>
     </>
   )
 }
--- client/src/pages/Transaction.jsx
+++ client/src/pages/Transaction.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom"
+
 export default function Transaction() {
+    const { boxId } = useParams();
+
     return (
         <div>
-            <p>Transaction Page</p>
+            <p>Transaction Page - with box id {boxId}</p>
         </div>
     )
 }
