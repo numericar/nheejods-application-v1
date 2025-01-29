78717faa4dcc93ba6161d85497698f5a4ceb5f6e
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-29 16:05:54 +0700

    item component

--- client/src/components/boxs/Item.jsx
+++ client/src/components/boxs/Item.jsx
@@ -1,7 +1,28 @@
-export default function Item() {
+import { FaCircle } from "react-icons/fa"
+import RickLevel from "./RickLevel";
+
+export default function Item({ boxItem }) {
     return (
-        <diV>
-            <p>Box item component</p>
-        </diV>
+        <div className="border-solid border-1 border-gray-500 p-3 rounded-lg flex flex-col items-center gap-3 cursor-pointer hover:text-white hover:bg-linear-to-r hover:from-orange-400/70 hover:to-blue-500/70">
+            <h1 className="font-bold text-lg">{boxItem.month}</h1>
+            <div className="flex flex-col items-center">
+                <span>ยอดคงเหลือ</span>
+                <span className="font-bold text-2xl">{boxItem.remaining}</span>
+            </div>
+            <div className="flex justify-between w-50">
+                <div className="flex items-center gap-2 text-green-600">
+                    <FaCircle />
+                    <span>{boxItem.incomeSummary}</span>
+                </div>
+                <div className="flex items-center gap-2 text-red-600">
+                    <FaCircle />
+                    <span>{boxItem.expensesSummary}</span>
+                </div>
+            </div>
+            <div>
+                <RickLevel />
+                <p className="text-center font-bold text-2xl">30.00%</p>
+            </div>
+        </div>
     )
 }
--- client/src/pages/Boxs.jsx
+++ client/src/pages/Boxs.jsx
@@ -1,7 +1,42 @@
 import SummaryFinancial from "../components/shared/SummaryFinancial";
 import Filter from "../components/boxs/Filter";
+import Item from "../components/boxs/Item";
 
 export default function Boxs() {
+    const boxs = [{
+        boxId: 1,
+        month: "มกราคม",
+        remaining: 20000,
+        incomeSummary: 30000,
+        expensesSummary: 10000,
+        rickLevel: 2,
+        expensesPercen: 30
+    },{
+        boxId: 2,
+        month: "กุมภาพันธ์",
+        remaining: 20000,
+        incomeSummary: 30000,
+        expensesSummary: 10000,
+        rickLevel: 2,
+        expensesPercen: 30
+    },{
+        boxId: 3,
+        month: "มีนาคม",
+        remaining: 20000,
+        incomeSummary: 30000,
+        expensesSummary: 10000,
+        rickLevel: 2,
+        expensesPercen: 30
+    },{
+        boxId: 4,
+        month: "เมษายน",
+        remaining: 20000,
+        incomeSummary: 30000,
+        expensesSummary: 10000,
+        rickLevel: 2,
+        expensesPercen: 30
+    }];
+
     return (
         <div className="container mx-auto mt-8">
             <div className="flex justify-between items-end">
@@ -13,7 +48,11 @@ export default function Boxs() {
                 </div>
             </div>
             <hr className="my-5 text-gray-400" />
-            <p>Boxs Page</p>
+            <div className="grid grid-cols-4 gap-5 xl:grid-cols-5">
+                {boxs && boxs.map((item) => (
+                    <Item key={item.boxId} boxItem={item} />
+                ))}
+            </div>
         </div>
     );
 }
