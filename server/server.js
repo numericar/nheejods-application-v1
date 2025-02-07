4563cf442d931bd28d58d4a09a5f186a6354bb5e
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-07 14:54:27 +0700

    initial server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(require("./middlewares/mapRoutes.middleware"));
+
+const PORT = process.env.PORT;
+if (PORT) {
+    app.listen(PORT, () => {
+        console.log(`Server start at http://localhost:${PORT}`);
+    }); 
+} else {
+    throw new Error("Server initialization failed. PORT not configured.")
+}
